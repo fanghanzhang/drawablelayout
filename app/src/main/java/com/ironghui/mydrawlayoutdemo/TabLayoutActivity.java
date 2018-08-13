ec913fb97cbfaabcd0f3706aac9c3083d1e4e12d
fanghanzhang <handler_zhang@163.com>
2018-08-13 11:50:37 +0800

    添加TabLayoutActivity

---
+++ app/src/main/java/com/ironghui/mydrawlayoutdemo/TabLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.ironghui.mydrawlayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TabLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
