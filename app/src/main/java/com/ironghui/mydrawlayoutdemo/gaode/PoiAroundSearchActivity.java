527140b57223f4d10b96ebb913a237b3a1dc9c22
fanghanzhang <handler_zhang@163.com>
2018-08-15 17:06:06 +0800

    添加高德地图关键字搜索

--- app/src/main/java/com/ironghui/mydrawlayoutdemo/gaode/PoiAroundSearchActivity.java
+++ app/src/main/java/com/ironghui/mydrawlayoutdemo/gaode/PoiAroundSearchActivity.java
@@ -1,12 +1,416 @@
 package com.ironghui.mydrawlayoutdemo.gaode;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PoiAroundSearchActivity extends AppCompatActivity {
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptor;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.CircleOptions;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.LatLngBounds;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+import com.ironghui.mydrawlayoutdemo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class PoiAroundSearchActivity extends AppCompatActivity implements PoiSearch.OnPoiSearchListener, AMap.OnMapClickListener, AMap.OnMarkerClickListener {
+    @BindView(R.id.mapView)
+    MapView mapView;
+    @BindView(R.id.btn_search)
+    TextView btnSearch;
+    @BindView(R.id.input_edittext)
+    EditText inputEdittext;
+    @BindView(R.id.search_bar_layout)
+    RelativeLayout searchBarLayout;
+    @BindView(R.id.poi_name)
+    TextView poiName;
+    @BindView(R.id.poi_address)
+    TextView poiAddress;
+    @BindView(R.id.poi_info)
+    TextView poiInfo;
+    @BindView(R.id.poi_detail)
+    RelativeLayout poiDetail;
+    private AMap mAMap;
+    private LatLonPoint lp = new LatLonPoint(39.993167, 116.473274);//
+    private String keyWord;
+    private PoiSearch.Query query;
+    private PoiSearch poiSearch;
+    private PoiResult poiResult;
+    private List<PoiItem> poiItems;// poi数据
+    private Marker mlastMarker;
+    private Marker locationMarker; // 选择的点
+    private Marker detailMarker;
+    private myPoiOverlay poiOverlay;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poiaroundsearch);
+        ButterKnife.bind(this);
+        mapView.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        if (mAMap == null) {
+            mAMap = mapView.getMap();
+            mAMap.setOnMapClickListener(this);
+            mAMap.setOnMarkerClickListener(this);
+            locationMarker = mAMap.addMarker(new MarkerOptions()
+                    .anchor(0.5f, 0.5f)
+                    .icon(BitmapDescriptorFactory
+                            .fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.point4)))
+                    .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+        }
+        mAMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lp.getLatitude(), lp.getLongitude()), 14));
+    }
+
+
+    @OnClick(R.id.btn_search)
+    public void onViewClicked() {
+        keyWord = btnSearch.getText().toString().trim();
+        if (null != keyWord) {
+            doSearchQuery();
+        }
+    }
+
+    private void doSearchQuery() {
+        int currentPage = 0;
+        // 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query = new PoiSearch.Query(keyWord, "", "北京市");
+        query.setPageSize(20);// 设置每页最多返回多少条poiitem
+        query.setPageNum(currentPage);// 设置查第一页
+
+        if (lp != null) {
+            poiSearch = new PoiSearch(this, query);
+            poiSearch.setOnPoiSearchListener(this);
+            poiSearch.setBound(new PoiSearch.SearchBound(lp, 5000, true));//
+            // 设置搜索区域为以lp点为圆心，其周围5000米范围
+            poiSearch.searchPOIAsyn();// 异步搜索
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (marker.getObject() != null) {
+            whetherToShowDetailInfo(true);
+            try {
+                PoiItem mCurrentPoi = (PoiItem) marker.getObject();
+                if (mlastMarker == null) {
+                    mlastMarker = marker;
+                } else {
+                    // 将之前被点击的marker置为原来的状态
+                    resetlastmarker();
+                    mlastMarker = marker;
+                }
+                detailMarker = marker;
+                detailMarker.setIcon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.poi_marker_pressed)));
+                setPoiItemDisplayContent(mCurrentPoi);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        } else {
+            whetherToShowDetailInfo(false);
+            resetlastmarker();
+        }
+        return true;
+    }
+
+    private void setPoiItemDisplayContent(PoiItem mCurrentPoi) {
+        poiName.setText(mCurrentPoi.getTitle());
+        poiAddress.setText(mCurrentPoi.getSnippet());
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        whetherToShowDetailInfo(false);
+        if (mlastMarker != null) {
+            resetlastmarker();
+        }
+    }
+
+    private void resetlastmarker() {
+        int index = poiOverlay.getPoiIndex(mlastMarker);
+        if (index < 10) {
+            mlastMarker.setIcon(BitmapDescriptorFactory
+                    .fromBitmap(BitmapFactory.decodeResource(
+                            getResources(),
+                            markers[index])));
+        } else {
+            mlastMarker.setIcon(BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight)));
+        }
+        mlastMarker = null;
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+        whetherToShowDetailInfo(false);
+    }
+
+    private void whetherToShowDetailInfo(boolean isToShow) {
+        if (isToShow) {
+            poiDetail.setVisibility(View.VISIBLE);
+
+        } else {
+            poiDetail.setVisibility(View.GONE);
+
+        }
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    private int[] markers = {R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10
+    };
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+                    if (poiItems != null && poiItems.size() > 0) {
+                        //清除POI信息显示
+                        whetherToShowDetailInfo(false);
+                        //并还原点击marker样式
+                        if (mlastMarker != null) {
+//                            resetlastmarker();
+                        }
+                        //清理之前搜索结果的marker
+                        if (poiOverlay != null) {
+                            poiOverlay.removeFromMap();
+                        }
+                        mAMap.clear();
+                        poiOverlay = new myPoiOverlay(mAMap, poiItems);
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+
+                        mAMap.addMarker(new MarkerOptions()
+                                .anchor(0.5f, 0.5f)
+                                .icon(BitmapDescriptorFactory
+                                        .fromBitmap(BitmapFactory.decodeResource(
+                                                getResources(), R.drawable.point4)))
+                                .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+
+                        mAMap.addCircle(new CircleOptions()
+                                .center(new LatLng(lp.getLatitude(),
+                                        lp.getLongitude())).radius(5000)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.argb(50, 1, 1, 1))
+                                .strokeWidth(2));
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+                        Toast.makeText(this, "无结果", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } else {
+
+                Toast.makeText(this, "无结果", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, rcode, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    private class myPoiOverlay {
+        private AMap mamap;
+        private List<PoiItem> mPois;
+        private ArrayList<Marker> mPoiMarks = new ArrayList<Marker>();
+
+        public myPoiOverlay(AMap amap, List<PoiItem> pois) {
+            mamap = amap;
+            mPois = pois;
+        }
+
+        /**
+         * 添加Marker到地图中。
+         *
+         * @since V2.1.0
+         */
+        public void addToMap() {
+            for (int i = 0; i < mPois.size(); i++) {
+                Marker marker = mamap.addMarker(getMarkerOptions(i));
+                PoiItem item = mPois.get(i);
+                marker.setObject(item);
+                mPoiMarks.add(marker);
+            }
+        }
+
+        /**
+         * 去掉PoiOverlay上所有的Marker。
+         *
+         * @since V2.1.0
+         */
+        public void removeFromMap() {
+            for (Marker mark : mPoiMarks) {
+                mark.remove();
+            }
+        }
+
+        /**
+         * 移动镜头到当前的视角。
+         *
+         * @since V2.1.0
+         */
+        public void zoomToSpan() {
+            if (mPois != null && mPois.size() > 0) {
+                if (mamap == null)
+                    return;
+                LatLngBounds bounds = getLatLngBounds();
+                mamap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            }
+        }
+
+        private LatLngBounds getLatLngBounds() {
+            LatLngBounds.Builder b = LatLngBounds.builder();
+            for (int i = 0; i < mPois.size(); i++) {
+                b.include(new LatLng(mPois.get(i).getLatLonPoint().getLatitude(),
+                        mPois.get(i).getLatLonPoint().getLongitude()));
+            }
+            return b.build();
+        }
+
+        private MarkerOptions getMarkerOptions(int index) {
+            return new MarkerOptions()
+                    .position(
+                            new LatLng(mPois.get(index).getLatLonPoint()
+                                    .getLatitude(), mPois.get(index)
+                                    .getLatLonPoint().getLongitude()))
+                    .title(getTitle(index)).snippet(getSnippet(index))
+                    .icon(getBitmapDescriptor(index));
+        }
+
+        protected String getTitle(int index) {
+            return mPois.get(index).getTitle();
+        }
+
+        protected String getSnippet(int index) {
+            return mPois.get(index).getSnippet();
+        }
+
+        /**
+         * 从marker中得到poi在list的位置。
+         *
+         * @param marker 一个标记的对象。
+         * @return 返回该marker对应的poi在list的位置。
+         * @since V2.1.0
+         */
+        public int getPoiIndex(Marker marker) {
+            for (int i = 0; i < mPoiMarks.size(); i++) {
+                if (mPoiMarks.get(i).equals(marker)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /**
+         * 返回第index的poi的信息。
+         *
+         * @param index 第几个poi。
+         * @return poi的信息。poi对象详见搜索服务模块的基础核心包（com.amap.api.services.core）中的类 <strong><a href="../../../../../../Search/com/amap/api/services/core/PoiItem.html" title="com.amap.api.services.core中的类">PoiItem</a></strong>。
+         * @since V2.1.0
+         */
+        public PoiItem getPoiItem(int index) {
+            if (index < 0 || index >= mPois.size()) {
+                return null;
+            }
+            return mPois.get(index);
+        }
+
+        protected BitmapDescriptor getBitmapDescriptor(int arg0) {
+            if (arg0 < 10) {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), markers[arg0]));
+                return icon;
+            } else {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight));
+                return icon;
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/ironghui/mydrawlayoutdemo/gaode/PoiWordsActivity.java
@@ -0,0 +1,15 @@
+package com.ironghui.mydrawlayoutdemo.gaode;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ironghui.mydrawlayoutdemo.R;
+
+public class PoiWordsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poiwords);
+    }
+}
